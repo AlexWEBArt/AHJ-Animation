ce76bdc7af5684d60633cd2b1ea03628eb773646
Артемчук Алексей <elusiv@mail.ru>
2023-03-18 14:20:27 +0600

    mobile update 18.03.23

--- src/js/WidgetCollapse.js
+++ src/js/WidgetCollapse.js
@@ -3,45 +3,6 @@ export default class WidgetCollapse {
     this.container = container;
   }
 
-  openChat() {
-    const chatContainer = document.createElement('DIV');
-    const formChat = document.createElement('FORM');
-    const titleChat = document.createElement('P');
-    const textareaChat = document.createElement('TEXTAREA');
-    const sendChat = document.createElement('BUTTON');
-    const closeChat = document.createElement('SPAN');
-
-    chatContainer.classList.add('container-chat');
-    formChat.classList.add('form-chat');
-    titleChat.classList.add('title-chat');
-    textareaChat.classList.add('textarea-chat');
-    sendChat.classList.add('send-chat');
-    closeChat.classList.add('close-chat');
-
-    titleChat.textContent = 'Напишите нам';
-    sendChat.textContent = 'Отправить';
-    closeChat.textContent = '\u{2716}';
-
-    textareaChat.spellCheck = 'false';
-
-    this.container.appendChild(chatContainer);
-    chatContainer.append(titleChat);
-    chatContainer.append(formChat);
-    chatContainer.append(closeChat);
-    formChat.append(textareaChat);
-    formChat.append(sendChat);
-
-    closeChat.addEventListener('click', () => {
-      chatContainer.classList.add('disapperance');
-      document.querySelector('.open-callback-chat').classList.remove('toPoint');
-      document.querySelector('.open-callback-chat').classList.add('fromPoint');
-
-      chatContainer.addEventListener('animationend', () => {
-        chatContainer.remove();
-      });
-    });
-  }
-
   openAnimationPage() {
     const collapseContainer = document.createElement('DIV');
     const btnCollapse = document.createElement('BUTTON');
